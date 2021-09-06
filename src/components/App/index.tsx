e80bd0e73ac41ccc3d3cb4bee7fbbf1cff5f55c4
Ruslan Orazliev <rorazliev@outlook.com>
2021-09-06 18:49:23 -0500

    Handle unhandled rejections

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,16 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import ignoreRejection from '../../helpers/ignoreRejection';
 
-const App: React.FC = (): ReactElement => <div />;
+const App: React.FC = (): ReactElement => {
+  // Handle unhandled rejections
+  useEffect(() => {
+    window.addEventListener('unhandledrejection', ignoreRejection);
+    return () => {
+      window.removeEventListener('unhandledrejection', ignoreRejection);
+    };
+  });
+
+  return <div />;
+};
 
 export default App;
